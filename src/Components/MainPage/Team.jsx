effbcc0e577916286189619c3fe94d7edaad3a0b
ARehman <abdurrehman1722@gmail.com>
2024-06-30 18:52:11 +0500

    teams updated

--- src/Components/MainPage/Team.jsx
+++ src/Components/MainPage/Team.jsx
@@ -11,7 +11,7 @@ const Team = () => {
     <div className="flex flex-wrap -m-2">
       <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
         <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-          <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src=".\public\images\main\arehman.png"/>
+          <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="\images\main\arehman.png"/>
           <div className="flex-grow">
             <h2 className="text-gray-900 title-font font-medium">A Rehman </h2>
             <p className="text-gray-500">Software engineer</p>
@@ -20,7 +20,7 @@ const Team = () => {
       </div>
       <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
         <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-          <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src=".\public\images\main\usama.jpg"/>
+          <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="\images\main\usama.jpg"/>
           <div className="flex-grow">
             <h2 className="text-gray-900 title-font font-medium">Usama Attique</h2>
             <p className="text-gray-500">Web Engineer</p>
@@ -29,7 +29,7 @@ const Team = () => {
       </div>
       <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
         <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-          <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src=".\public\images\main\usman.png"/>
+          <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="\images\main\usman.png"/>
           <div className="flex-grow">
             <h2 className="text-gray-900 title-font font-medium">Usman Zafar</h2>
             <p className="text-gray-500">Mobile App Developer</p>
